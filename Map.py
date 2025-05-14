500f52b324acda1c0337d7f55a88960e7d12bc4d
dylanglenister <117338067+DylanGlenister@users.noreply.github.com>
2025-05-14 17:10:44 +1000

    Added some stuff with map

--- Map.py
+++ Map.py
@@ -1,6 +1,6 @@
 import folium as fm
 Start = -37.80486 + 0.00142, 145.08093 + 0.00171
-Connect = -37.82526 + 0.00142, 145.07758 + 0.00171 
+Connect = -37.82526 + 0.00142, 145.07758 + 0.00171
 End = -37.82371 + 0.00142, 145.06418 + 0.00171
 # Create the map
 m = fm.Map(location=Start, zoom_start = 15)
