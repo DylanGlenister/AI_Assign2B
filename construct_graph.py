0098a1ee09ea286c5409517710c2666bb1c82da5
dylanglenister <117338067+DylanGlenister@users.noreply.github.com>
2025-05-23 01:51:20 +1000

    Massively improved graph creation. Using the edge lookup makes things super simple and easy.

--- construct_graph.py
+++ construct_graph.py
@@ -5,38 +5,37 @@ import folium as fm
 
 import search
 
-def create_graph(_df: pd.DataFrame, _debug = False):
-	'''Programmatically uses the information from the dataset to construct the graph.'''
-
+def get_edge_lookup():
+	'''Retrieves the data from the edge lookup file. Format is {(end, dir), start}.'''
 	edge_lookup: dict[tuple[int, str], int] = {}
 	with open('./edge_lookup.txt', 'r') as file:
 		for line in file.readlines():
 			end, dir, start = line.strip().split(',')
 			edge_lookup[(int(end), dir)] = int(start)
+	return edge_lookup
+
+def graph_visualisation(_df: pd.DataFrame):
+	'''Creates an openstreetmap file that displays the node/edge information.'''
+
+	edge_lookup = get_edge_lookup()
 
 	m = fm.Map(location=(-37.82, 145.07), zoom_start=13)
 
 	# Reduce the dataframe to only the needed unformation
-	sites = _df[['SCATS', 'Intersection', 'Direction', 'Latitude', 'Longitude']].copy().drop_duplicates()
+	sites = _df[['SCATS', 'Direction', 'Latitude', 'Longitude']].copy().drop_duplicates()
 
-	#scat_locations: dict[int, tuple[float, float]] = {}
 	site_locations: dict[tuple[int, str], tuple[float, float]] = {}
-	street_to_nodes: dict[str, list[int]] = {}
 
 	for _, row in sites.iterrows():
 		scats: int = row['SCATS']
 		latitude: float = row['Latitude']
 		longitude: float = row['Longitude']
-		intersection: str = row['Intersection']
 		direction: str = row['Direction']
-
-		# Locations is easy to set up
-		#scat_locations[scats] = (latitude, longitude)
 
 		site_locations[(scats, direction)] = (latitude, longitude)
 
 		# Find if a site is present in the lookup table
-		present = any(end == scats and dir == direction for (end, dir), start in edge_lookup.items())
+		present = any(end == scats and dir == direction for (end, dir), _ in edge_lookup.items())
 
 		# Display the sites on the map, these are the edges
 		fm.Marker(
@@ -45,18 +44,6 @@ def create_graph(_df: pd.DataFrame, _debug = False):
 			icon=fm.Icon(color='red' if present else 'blue')
 		).add_to(m)
 
-		streets = [street.strip() for street in intersection.split('/')]
-
-		# Associate each street with the SCATS number
-		for street in streets:
-			if street:
-				if street not in street_to_nodes:
-					street_to_nodes[street] = []
-				street_to_nodes[street].append(scats)
-
-	# Add the edges to the graph
-
-	# Average the locations
 	grouped = sites[['SCATS', 'Latitude', 'Longitude']].groupby('SCATS').mean()
 	average_position = {scat: (pos['Latitude'], pos['Longitude']) for scat, pos in grouped.iterrows()}
 
@@ -65,18 +52,29 @@ def create_graph(_df: pd.DataFrame, _debug = False):
 
 	m.save('graph.html')
 
-	edge_dict = defaultdict(lambda: defaultdict(int))  # Nested defaultdict for {node: {connected_node: cost}}
+def create_graph(_df: pd.DataFrame, _debug = False):
+	'''Programmatically uses the information from the dataset to construct the graph.'''
 
-	# Connect a node to all other nodes with the same street
-	for _, nodes in street_to_nodes.items():
-		for node in nodes:
-			# Add all other nodes from this street as edges with default cost 1
-			for connected_node in nodes:
-				if connected_node != node:
-					edge_dict[node][connected_node] = 1
+	edge_lookup = get_edge_lookup()
 
-	# Convert to a regular dictionary
-	edges: dict[int, dict[int, int]] = {node: dict(connected_nodes) for node, connected_nodes in edge_dict.items()}
+	# Reduce the dataframe to only the needed unformation
+	sites = _df[['SCATS', 'Direction', 'Latitude', 'Longitude']].copy().drop_duplicates()
+
+	# Average the locations
+	grouped = sites[['SCATS', 'Latitude', 'Longitude']].groupby('SCATS').mean()
+	average_position = {scat: (pos['Latitude'], pos['Longitude']) for scat, pos in grouped.iterrows()}
+
+	# Edge lookup will be used to query the model for the cost
+
+	# Add the edges to the graph
+	# Edges need to be in the format {start: {end, cost}}
+
+	edges: dict[int, dict[int, int]] = {}
+
+	for (end, _), start in edge_lookup.items():
+		if start not in edges:
+			edges[start] = {}
+		edges[start][end] = 1
 
 	if _debug:
 		# Print basic graph info
@@ -97,6 +95,7 @@ def create_graph(_df: pd.DataFrame, _debug = False):
 
 def test():
 	processed_data = pd.read_csv('./processed.csv')
+	graph_visualisation(processed_data)
 
 	graph, _ = create_graph(processed_data)
 
